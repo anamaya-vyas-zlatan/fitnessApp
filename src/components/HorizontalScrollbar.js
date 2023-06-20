e17d80a45c1c2420ad755a52ba9047af35b5c6c2
Vyas <anamaya.vyas@hpe.com>
2023-06-20 16:15:40 +0530

    API Working

---
+++ src/components/HorizontalScrollbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Box} from '@mui/material'
+const HorizontalScrollbar = ({data}) => {
+  return (
+    <div>
+      HorizontalScrollbar
+    </div>
+  )
+}
+
+export default HorizontalScrollbar
